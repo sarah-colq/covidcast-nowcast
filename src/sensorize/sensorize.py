b348a504650a7452020c6ea03ceb775fd077c459
andrew <chinandrew96@gmail.com>
2021-01-05 13:20:07 -0800

    add nowcast and sensorize subfolders

---
+++ src/sensorize/sensorize.py
@@ -0,0 +1,16 @@
+"""Sensorize and export as CSVs."""
+
+
+def main():
+    """
+    Run sensorization code and export data for ingestion.
+
+    Returns
+    -------
+        None
+    """
+    # read config/constants file
+    # download data
+    # generate sensors
+    # export CSVs
+    pass
