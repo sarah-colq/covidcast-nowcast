dadf5a58ce8412525c480e55897251538d43489f
andrew <chinandrew96@gmail.com>
2021-01-13 09:00:28 -0800

    Add basic regression model

--- delphi_covidcast_nowcast/sensorization/ar_model.py
+++ delphi_covidcast_nowcast/sensorization/ar_model.py
@@ -7,8 +7,36 @@ def compute_ar_sensor(date: int,
                       values: LocationSeries,
                       ar_size=2,
                       include_intercept=False,
-                      lambda_=0.1)-> float:
-    """Fit model and get sensorization value for a given date."""
+                      lambda_=0.1) -> float:
+    """
+    Fit AR model and get sensorization value for a given date.
+
+    This takes in two LocationSeries objects for the predictor and response as well as a date to
+    predict and some model parameters. The model is trained on all data before the specified date,
+    and then the predictor at the given date is fed into the model to get the returned sensor value
+    for that day.
+
+    For now, this function assumes there are no gaps in the data.
+
+    It does not normalize the data yet.
+
+    Parameters
+    ----------
+    date
+        date to get sensor value for
+    covariate
+        LocationSeries containing covariate values.
+    response
+        LocationSeries containing response values.
+    include_intercept
+        Boolean on whether or not to include intercept.
+    lambda_
+        l2 regularization coefficient
+
+    Returns
+    -------
+        Float value of sensor on `date`
+    """
     # fill in gaps in data if any missing dates (e.g. polynomial imputation)?
     idx = values.dates.index(date)
     Yhat = ar_predict(idx, np.array(values.values), ar_size, include_intercept, lambda_)[1]
--- delphi_covidcast_nowcast/sensorization/regression_model.py
+++ delphi_covidcast_nowcast/sensorization/regression_model.py
@@ -1,12 +1,50 @@
+import numpy as np
+
 from ..data_containers import LocationSeries
 
 
 def compute_regression_sensor(date: int,
-                              values: LocationSeries,
+                              covariate: LocationSeries,
                               response: LocationSeries,
-                              other_params=None) -> float:
-    """Fit model and get sensorization value for a given date."""
-    # select dates required for training and predicting a value for "date" e.g. last 60 days
+                              include_intercept=False,
+                              lambda_=0.1) -> float:
+    """
+    Fit regression model and get sensorization value for a given date.
+
+    This takes two LocationSeries objects for a covariate and response as well as a date to
+    predict and some model parameters. The model is trained on all data before the specified date,
+    and then the predictor at the given date is fed into the model to get the returned sensor value
+    for that day.
+
+    For now, this function assumes there are no gaps in the data.
+
+    It does not normalize the data yet.
+
+    Parameters
+    ----------
+    date
+        date to get sensor value for
+    covariate
+        LocationSeries containing covariate values.
+    response
+        LocationSeries containing response values.
+    include_intercept
+        Boolean on whether or not to include intercept.
+    lambda_
+        l2 regularization coefficient
+
+    Returns
+    -------
+        Float value of sensor on `date`
+    """
+    # fill in gaps in data if any missing dates (e.g. polynomial imputation)?
+    idx = covariate.dates.index(date)
+    train_Y = response.values[:idx]
+    train_X_covariates = covariate.values[:idx]
     # error if not enough data in either values or response
-    # for specified date, compute the sensorization value and return it
-    pass
+
+    predict_X = np.hstack((1, covariate.values[idx])) if include_intercept else covariate.values[idx]
+    X = np.ones((len(train_X_covariates), 1 + include_intercept))
+    X[:, -1] = train_X_covariates
+    B = np.linalg.inv(X.T @ X + lambda_ * np.eye(1 + include_intercept)) @ X.T @ train_Y
+    return predict_X @ B
