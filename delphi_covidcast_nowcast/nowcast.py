c30f8974295d6e0026e2cf5751a53c2a87e8436b
Maria Jahja <mariajahja@gmail.com>
2021-01-14 02:06:02 -0500

    sensor values to mat + add fuse code + fips tables

--- delphi_covidcast_nowcast/nowcast.py
+++ delphi_covidcast_nowcast/nowcast.py
@@ -4,8 +4,10 @@ from typing import List, Tuple
 
 import numpy as np
 
-from .deconvolution.deconvolution import deconvolve_signal
-from .data_containers import SignalConfig
+from delphi_covidcast_nowcast.data_containers import SignalConfig
+from delphi_covidcast_nowcast.deconvolution.deconvolution import deconvolve_signal
+from delphi_covidcast_nowcast.nowcast_fusion import covariance, fusion
+from delphi_covidcast_nowcast.sensorization.sensor import get_sensors
 
 
 def nowcast(input_dates: List[int],
@@ -43,8 +45,54 @@ def nowcast(input_dates: List[int],
                                      input_locations, np.array(kernel))
 
     # fit sensors
+    train_sensors = get_sensors(input_dates[0], input_dates[-1],
+                                sensor_indicators, ground_truth,
+                                # ground_truth = None if compute_missing=False
+                                compute_missing=True)  # change to false once we have DB
+
+    now_sensors = get_sensors(nowcast_dates[0], nowcast_dates[0],
+                              sensor_indicators, ground_truth,
+                              compute_missing=True)
+
+    ## put into matrix form
+    # convert to dict indexed by loc to make matching across train/now easier
+    y = dict((s.geo_value, s) for s in ground_truth)
+    n_sensor_locs = len(sensor_indicators) * len(input_locations)
+    noise = np.full((len(input_dates), n_sensor_locs), np.nan)
+    z = np.full((1, n_sensor_locs), np.nan)
+    j = 0
+    for sensor in sensor_indicators:
+        # convert to dict indexed by loc to make matching across train/now easier
+        train_series = dict((s.geo_value, s) for s in train_sensors[sensor])
+        now_series = dict((s.geo_value, s) for s in now_sensors[sensor])
+        valid_sensor_locs = set(train_series.keys()) & set(now_series.keys())
+
+        for loc in sorted(valid_sensor_locs):
+            noise[:, j] = y[loc].values - train_series[loc].values
+            z[:, j] = now_series[loc].values
+            j += 1
+
+    # cull nan columns
+    finite_cols = np.logical_and(np.all(np.isfinite(noise), axis=0),
+                                 np.all(np.isfinite(z), axis=0))
+    noise = noise[:, finite_cols]
+    z = z[:, finite_cols]
+
+    print(noise.shape, z.shape)
+
     # generate statespace
+    # to do
+    H = np.ones((noise.shape[1], len(ground_truth))) / len(ground_truth)
+    H[0, 0] = 1
+    W = np.ones((len(ground_truth), len(ground_truth))) / len(ground_truth)
+
     # estimate covariance
+    R = covariance.mle_cov(noise, covariance.BlendDiagonal2)
+
     # run SF
+    x, P = fusion.fuse(z, R, H)
+    y, S = fusion.extract(x, P, W)
+    stdev = np.sqrt(np.diag(S)).reshape(y.shape)
+
     # return output
     pass
--- delphi_covidcast_nowcast/run_example.py
+++ delphi_covidcast_nowcast/run_example.py
@@ -2,16 +2,16 @@
 
 import delphi_covidcast_nowcast.deconvolution.delay_kernel as delay
 import delphi_covidcast_nowcast.nowcast as nowcast
-
+from delphi_covidcast_nowcast.data_containers import SignalConfig
 
 def main():
     input_dates = list(range(20200601, 20200610))
     input_locations = [('pa', 'state'),
                        ('42003', 'county')]
-    sensor_indicators = [('auto-reg', 'ar3'),
-                         ('fb-survey', 'smoothed_hh_cmnty_cli'),
-                         ('doctor-visits', 'smoothed_adj_cli')]
-    convolved_truth_indicator = ('usa-facts', 'confirmed_incidence_num')
+    sensor_indicators = [SignalConfig('usa-facts', 'confirmed_incidence_num', 'ar3', 'ar'),
+                         SignalConfig('fb-survey', 'smoothed_hh_cmnty_cli', 'fb', 'regression')]
+    convolved_truth_indicator = SignalConfig(source='usa-facts',
+                                             signal='confirmed_incidence_num')
     kernel = delay.get_florida_delay_distribution()
     nowcast_dates = [20200610]
 
