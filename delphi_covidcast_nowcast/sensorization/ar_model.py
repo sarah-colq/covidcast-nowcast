d11963a2b0431472315d217a8fd572a85ae1fa7e
andrew <chinandrew96@gmail.com>
2021-01-20 07:51:22 -0800

    Add standardization

--- delphi_covidcast_nowcast/sensorization/ar_model.py
+++ delphi_covidcast_nowcast/sensorization/ar_model.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from datetime import datetime, timedelta
 
 import numpy as np
@@ -9,7 +10,8 @@ def compute_ar_sensor(day: int,
                       values: LocationSeries,
                       ar_size: int = 2,
                       include_intercept: bool = False,
-                      lambda_: float = 0.1) -> float:
+                      lambda_: float = 0.1,
+                      standardize: bool = True) -> float:
     """
     Fit AR model through least squares and get sensorization value for a given date.
 
@@ -34,19 +36,27 @@ def compute_ar_sensor(day: int,
     include_intercept
         Boolean on whether or not to include intercept.
     lambda_
-        l2 regularization coefficient
+        l2 regularization coefficient.
+    standardize
+        Boolean for whether or not to standardize the data before fitting. If True,
+        include_intercept is is set to False.
 
     Returns
     -------
         Float value of sensor on `date`
     """
+    if standardize:
+        include_intercept = False
     previous_day = int((datetime.strptime(str(day), "%Y%m%d") - timedelta(1)).strftime("%Y%m%d"))
     window = values.get_data_range(min(values.dates), previous_day)
-    B = _ar_fit(np.array(window), ar_size, include_intercept, lambda_)
+    B, means, stddevs = _ar_fit(np.array(window), ar_size, include_intercept, lambda_, standardize)
     if B is None:
         return np.nan
-    date_X = np.hstack((window[-ar_size:], 1)) if include_intercept else np.array(window[-ar_size:])
+    date_X = np.hstack((1, window[-ar_size:])) if include_intercept else np.array(window[-ar_size:])
+    date_X = (date_X - means) / stddevs if standardize else date_X
     Yhat = (date_X @ B)[0]
+
+    # Taken from https://github.com/dfarrow0/covidcast-nowcast/blob/dfarrow/sf/src/sf/ar_sensor.py:
     # ground truth in some locations is a zero vector, which leads to perfect AR fit, zero
     # variance, and a singular covariance matrix so as a small hack, add some small noise.
     np.random.seed(day)
@@ -58,45 +68,64 @@ def compute_ar_sensor(day: int,
     return Yhat
 
 
-def _ar_fit(values, ar_size, include_intercept, lambda_):
+def _ar_fit(values: np.array,
+            ar_size: int,
+            include_intercept: bool,
+            lambda_: float,
+            standardize: bool) -> Tuple[np.array, np.array, np.array]:
     """
-    Fit AR coefficients.
+    Fit AR coefficients with OLS.
 
-    Taken from https://github.com/dfarrow0/covidcast-nowcast/tree/dfarrow/sf/src/sf
-
-    TODO: L2 is implemented incorrectly. ideally covariates would be
-    normalized before adding the penalty (so as not to unfairly penalize
-    covariates with high variance), but here they're not being normalized.
-    probably doesn't matter too much for now, but something to fix later.
+    Adapted from
+    https://github.com/dfarrow0/covidcast-nowcast/blob/dfarrow/sf/src/sf/ar_sensor.py
 
     Parameters
     ----------
-    idx
     values
+        Array of values to train on.
     ar_size
+        Order of autoregressive model.
     include_intercept
+        Boolean for whether to fit an intercept or not.
     lambda_
+        l2 regularization coefficient.
+    standardize
+        Boolean for whether or not to standardize the data before fitting. If True,
+        include_intercept is is set to False.
+    Returns
+    -------
+        Tuple of (fitted coefficients, mean vector, stddev vector). If standardize is False,
+        mean will be 0 stddev vector will be 1, which will be a no-op.
+    """
+    if standardize:
+        include_intercept = False
+    num_covariates = ar_size + include_intercept
+    num_observations = len(values) - ar_size
+    if num_observations < 2 * num_covariates:  # require some minimum number of samples
+        return None, None, None
+    X = np.hstack([values[j:-(ar_size - j), None] for j in range(ar_size)])
+    if include_intercept:
+        X = np.hstack((np.ones((num_observations, 1)), X))
+    X, means, stddevs = _standardize(X) if standardize else (X, 0, 1)
+    Y = values[ar_size:, None]
+    B = np.linalg.inv(X.T @ X + lambda_ * np.eye(num_covariates)) @ X.T @ Y
+    return B, means, stddevs
+
+
+def _standardize(data: np.ndarray) -> Tuple[np.ndarray, np.array, np.array]:
+    """
+    Standardize a matrix and return the mean and stddevs for each column
+
+    Parameters
+    ----------
+    data
+        Numpy matrix to standardize
 
     Returns
     -------
-
+        Standardize matrix, mean vector, stddev vector.
     """
-    num_covariates = ar_size
-    if include_intercept:
-        num_covariates += 1
-    num_observations = len(values) - ar_size
-    if num_observations < 2 * num_covariates:
-        # require some minimum number of samples
-        return None
-
-    # fairly standard OLS, maybe with intercept, and with L2 penalty
-    X = np.zeros((num_observations, num_covariates))
-    X[:, -1] = include_intercept
-    for j in range(ar_size):
-        X[:, j] = values[j:-(ar_size - j)]
-    Y = values[ar_size:, None]
-    X = np.vstack((X, lambda_ * np.eye(num_covariates)))
-    Y = np.vstack((Y, np.zeros((num_covariates, 1))))
-    B = np.linalg.inv(X.T @ X) @ X.T @ Y
-    return B
-
+    means = np.mean(data, axis=0)
+    stddevs = np.std(data, axis=0)
+    data = (data - means) / stddevs
+    return data, means, stddevs
--- tests/sensorization/test_ar_model.py
+++ tests/sensorization/test_ar_model.py
@@ -19,11 +19,11 @@ class TestComputeRegressionSensor:
             dates=[20200101, 20200102, 20200103, 20200104, 20200105, 20200106, 20200107, 20200108,
                    20200109, 20200110, 20200111, 20200112, 20200113, 20200114, 20200115, 20200116])
         assert np.isclose(
-            compute_ar_sensor(20200115, values, 1, True, 0),
+            compute_ar_sensor(20200115, values, 1, True, 0, False),
             -0.09105891 + 0.87530957 * -0.02058535
         )
         assert np.isclose(
-            compute_ar_sensor(20200115, values, 2, True, 0),
+            compute_ar_sensor(20200115, values, 2, True, 0, False),
             0.31865395 + 0.64751725 * -0.02058535 + 0.30760218 * -1.77057154
         )
 
@@ -38,11 +38,11 @@ class TestComputeRegressionSensor:
             dates=[20200101, 20200102, 20200103, 20200104, 20200105, 20200106, 20200107, 20200108,
                    20200109, 20200110, 20200111, 20200112, 20200113, 20200114, 20200115, 20200116])
         assert np.isclose(
-            compute_ar_sensor(20200115, values, 1, False, 0),
+            compute_ar_sensor(20200115, values, 1, False, 0, False),
             0.90084256 * -0.02058535
         )
         assert np.isclose(
-            compute_ar_sensor(20200115, values, 2, False, 0),
+            compute_ar_sensor(20200115, values, 2, False, 0, False),
             0.61458575 * -0.02058535 + 0.25038242 * -1.77057154
         )
 
@@ -62,5 +62,41 @@ class TestComputeRegressionSensor:
             dates=[20200101, 20200102])
         assert np.isnan(compute_ar_sensor(20200102, values))
 
+    @patch("numpy.random.normal")
+    def test_compute_regression_sensor_standardize_intercept(self, random_normal):
+        """Test standardizing sets intercept to false."""
+        random_normal.return_value = 0
+        values = LocationSeries(
+            values=[-4.27815483, -4.83962077, -4.09548122, -3.86647783, -2.64494168, -3.99573135,
+                    -3.48248410, -2.77490127, -3.64162355, -2.57628910, -2.46793048, -3.20454941,
+                    -1.77057154, -0.02058535, 0.81182691, 0.32741982],
+            dates=[20200101, 20200102, 20200103, 20200104, 20200105, 20200106, 20200107, 20200108,
+                   20200109, 20200110, 20200111, 20200112, 20200113, 20200114, 20200115, 20200116])
+        assert np.isclose(
+            compute_ar_sensor(20200115, values, 1, True, 0, True),
+            compute_ar_sensor(20200115, values, 1, False, 0, True),
+        )
+
+    def test_compute_regression_sensor_standardize(self):
+        # TODO VALIDATE OUTPUT. Currently just runs them to make sure they don't break.
+        # the formulas we use look sound, but would be nice to have another package validate.
+        values = LocationSeries(
+            values=[-4.27815483, -4.83962077, -4.09548122, -3.86647783, -2.64494168, -3.99573135,
+                    -3.48248410, -2.77490127, -3.64162355, -2.57628910, -2.46793048, -3.20454941,
+                    -1.77057154, -0.02058535, 0.81182691, 0.32741982],
+            dates=[20200101, 20200102, 20200103, 20200104, 20200105, 20200106, 20200107, 20200108,
+                   20200109, 20200110, 20200111, 20200112, 20200113, 20200114, 20200115, 20200116])
+        compute_ar_sensor(20200115, values, 1, False, 0, True),
+        assert 1
+
     def test_compute_regression_sensor_regularize(self):
-        pass
+        # TODO VALIDATE OUTPUT. Currently just runs them to make sure they don't break.
+        # the formulas we use look sound, but would be nice to have another package validate.
+        values = LocationSeries(
+            values=[-4.27815483, -4.83962077, -4.09548122, -3.86647783, -2.64494168, -3.99573135,
+                    -3.48248410, -2.77490127, -3.64162355, -2.57628910, -2.46793048, -3.20454941,
+                    -1.77057154, -0.02058535, 0.81182691, 0.32741982],
+            dates=[20200101, 20200102, 20200103, 20200104, 20200105, 20200106, 20200107, 20200108,
+                   20200109, 20200110, 20200111, 20200112, 20200113, 20200114, 20200115, 20200116])
+        compute_ar_sensor(20200115, values, 1, False, 1, True),
+        assert 1
