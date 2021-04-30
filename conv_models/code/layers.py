a2efb9946a410bc98db341271b8c495fb46580a8
Stephen Sheng <stevexsheng@gmail.com>
2021-04-29 22:55:48 -0700

    added model with trained gamma distribution

--- conv_models/code/layers.py
+++ conv_models/code/layers.py
@@ -75,3 +75,77 @@ class CustomConv1D(Conv1D):
         if self.activation is not None:
             return self.activation(outputs)
         return outputs
+
+class CustomConvGamma1D(Conv1D):
+
+    def __init__(self, p, filters=2, activation=None, **kwargs):
+        super(CustomConvGamma1D, self).__init__(filters, kernel_size=1,
+                                           activation=None, use_bias=False, **kwargs)
+        self._kernel = tf.linspace(p,1,p)
+        self._kernel = tf.reshape(self._kernel, shape=(p,1,1))
+        self._kernel = tf.cast(
+            self._kernel, dtype=self.dtype)
+
+    def build(self, input_shape):
+        input_shape = tensor_shape.TensorShape(input_shape)
+        input_channel = self._get_input_channel(input_shape)
+        if input_channel % self.groups != 0:
+            raise ValueError(
+                'The number of input channels must be evenly divisible by the number '
+                'of groups. Received groups={}, but the input has {} channels '
+                '(full input shape is {}).'.format(self.groups, input_channel,
+                                                   input_shape))
+
+        kernel_shape = (1, 1, 1, self.filters)
+
+        self.kernel = self.add_weight(
+            name='kernel',
+            shape=kernel_shape,
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            constraint=self.kernel_constraint,
+            trainable=True,
+            dtype=self.dtype)
+
+        self.bias = None
+
+        channel_axis = self._get_channel_axis()
+        self.input_spec = InputSpec(min_ndim=self.rank + 2,
+                                    axes={channel_axis: input_channel})
+
+        # Convert Keras formats to TF native formats.
+        if self.padding == 'causal':
+            tf_padding = 'VALID'  # Causal padding handled in `call`.
+        elif isinstance(self.padding, six.string_types):
+            tf_padding = self.padding.upper()
+        else:
+            tf_padding = self.padding
+        tf_dilations = list(self.dilation_rate)
+        tf_strides = list(self.strides)
+
+        tf_op_name = self.__class__.__name__
+        if tf_op_name == 'Conv1D':
+            tf_op_name = 'conv1d'  # Backwards compat.
+
+        self._convolution_op = functools.partial(
+            nn_ops.convolution_v2,
+            strides=tf_strides,
+            padding=tf_padding,
+            dilations=tf_dilations,
+            data_format=self._tf_data_format,
+            name=tf_op_name)
+        self.built = True
+
+    def call(self, inputs):
+        alpha = self.kernel[0,0,0,0]
+        beta = self.kernel[0,0,0,1]
+
+        exp_beta = tf.math.exp(-beta*self._kernel)
+        x_alpha = self._kernel ** (alpha-1)
+        self.kernel_ = x_alpha * exp_beta
+        
+        outputs = self._convolution_op(inputs, self.kernel_)
+
+        if self.activation is not None:
+            return self.activation(outputs)
+        return outputs
--- conv_models/code/model.py
+++ conv_models/code/model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Conv1D, Lambda, Concatenate
-from layers import CustomConv1D
+from layers import CustomConv1D, CustomConvGamma1D
 
 
 class Model(tf.keras.Model):
@@ -29,23 +29,25 @@ class Model(tf.keras.Model):
         self.m = m
         self.kernel_constraint = kernel_constraint
         self.kernel_regularizer = kernel_regularizer
-        self.lam = lam # (NEW) used for the regularization part
+        self.lam = lam  # (NEW) used for the regularization part
         self.conv_layers = []
 
         if filter_bank:
-            self.uses_filter_bank = True # (NEW) used for the regularization part
+            # (NEW) used for the regularization part
+            self.uses_filter_bank = True
             for i in range(m):
                 layer = CustomConv1D(
                     filters=1,
                     filter_bank=filter_bank,
                     kernel_constraint=kernel_constraint,
                     kernel_regularizer=kernel_regularizer,
-                    name='custom_conv{}'.format(i),
+                    name='customConv{}'.format(i),
                 )
                 self.conv_layers.append(layer)
         else:
             for i in range(m):
-                self.uses_filter_bank = False # (NEW) used for the regularization part
+                # (NEW) used for the regularization part
+                self.uses_filter_bank = False
                 layer = Conv1D(
                     filters=1,
                     kernel_size=p,
@@ -91,16 +93,95 @@ class Model(tf.keras.Model):
 
                 for i in range(m):
                     # A_T is the set of filters stacked horizontally multiplied by their respective weights
-                    A_T = self.conv_layers[i].kernel * self.conv_layers[i].stacked_filter_bank # shape = (num_filters, length_of_each_filter)
+                    # shape = (num_filters, length_of_each_filter)
+                    A_T = self.conv_layers[i].kernel * \
+                        self.conv_layers[i].stacked_filter_bank
                     A_T = tf.squeeze(A_T)
-                    A = tf.transpose(A_T, perm=[1,0]) # shape = (length_of_each_filter, num_filters)
-                    K = tf.linalg.matmul(A_T,A) # shape = (num_filters, num_filters), this is effectively the Gram matrix of the set of filters [alpha_1*z_1, ..., alpha_i*z_i, ..., alpha_m*z_m]
+                    # shape = (length_of_each_filter, num_filters)
+                    A = tf.transpose(A_T, perm=[1, 0])
+                    # shape = (num_filters, num_filters), this is effectively the Gram matrix of the set of filters [alpha_1*z_1, ..., alpha_i*z_i, ..., alpha_m*z_m]
+                    K = tf.linalg.matmul(A_T, A)
                     L2 = tf.reduce_sum(K ** 2)
                     loss_regularizer += L2
-                
+
                 loss += self.lam*loss_regularizer
-            
+
             #####################################
+
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(
+            zip(gradients, self.trainable_variables))
+
+        for var in self.trainable_variables:
+            var.assign(tf.math.maximum(0., var))
+
+        self.compiled_metrics.update_state(Y, Y_hat)
+        return {m.name: m.result() for m in self.metrics}
+
+
+class ModelGamma(tf.keras.Model):
+    """
+    Simple convolutional model for fitting leading indicators to case counts
+    Attributes:
+        p: Size of the pd kernel
+        m (int): Number of geo_values
+    """
+
+    def __init__(self, p=30, m=1, kernel_constraint=None, kernel_regularizer=None, lam=1):
+        """
+        Args: 
+            p (int): Size of the p_d kernel. Value ignored when filter_bank given. 
+            m (int): Number of geo_values
+            filter_bank: A list of kernels. The kernels should be
+                1-dimensional arrays each with the correct orientation for
+                cross-correlation and length.
+        """
+        super(ModelGamma, self).__init__()
+        assert p > 0 and isinstance(
+            p, int), "p must be an integer greater than 0"
+
+        self.p = p
+        self.m = m
+        self.kernel_constraint = kernel_constraint
+        self.kernel_regularizer = kernel_regularizer
+        self.lam = lam
+        self.conv_layers = []
+
+        for i in range(m):
+            layer = CustomConvGamma1D(
+                p=p,
+                kernel_constraint=kernel_constraint,
+                kernel_initializer=tf.keras.initializers.Constant(value=2),
+                kernel_regularizer=kernel_regularizer,
+                name='customConvGamma{}'.format(i),
+            )
+            self.conv_layers.append(layer)
+
+    def call(self, x):
+        if self.m == 1:
+            return self.conv_layers[0](x)
+        else:
+            split_inputs = tf.split(x, num_or_size_splits=self.m, axis=-1)
+            split_outputs = [self.conv_layers[i](
+                split_inputs[i]) for i in range(self.m)]
+            joined_outputs = Concatenate()(split_outputs)
+            return joined_outputs
+
+    def train_step(self, inputs):
+        X, Y = inputs
+
+        assert X.shape[1] == Y.shape[1], "Size of X and Y should be the same shape but found, {} vs {}".format(
+            X.shape[1], Y.shape[1])
+        X_padded = tf.pad(
+            X,
+            paddings=[[0, 0], [self.p-1, 0], [0, 0]],
+        )
+
+        with tf.GradientTape() as tape:
+            Y_hat = self(X_padded)
+            loss = self.loss(Y, Y_hat)
+            for i in range(self.m):
+                loss -= self.lam * tf.reduce_sum(tf.math.log(self.conv_layers[i].kernel))
 
         gradients = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(
