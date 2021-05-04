1460b9d16ece3aaff968cc47e87a539228b06964
Stephen Sheng <stevexsheng@gmail.com>
2021-05-03 22:07:38 -0700

    added tests for taking differences between weights as a regularizer

--- conv_models/code/model.py
+++ conv_models/code/model.py
@@ -117,6 +117,112 @@ class Model(tf.keras.Model):
 
         self.compiled_metrics.update_state(Y, Y_hat)
         return {m.name: m.result() for m in self.metrics}
+    
+
+class ModelDiff(tf.keras.Model):
+    """
+    Simple convolutional model for fitting leading indicators to case counts
+    Attributes:
+        p: Size of the pd kernel
+        m (int): Number of geo_values
+    """
+
+    def __init__(self, p=30, m=1, kernel_constraint=None, kernel_regularizer=None, filter_bank=None, lam=0):
+        """
+        Args: 
+            p (int): Size of the p_d kernel. Value ignored when filter_bank given. 
+            m (int): Number of geo_values
+            filter_bank: A list of kernels. The kernels should be
+                1-dimensional arrays each with the correct orientation for
+                cross-correlation and length.
+        """
+        super(ModelDiff, self).__init__()
+        assert p > 0 and isinstance(
+            p, int), "p must be an integer greater than 0"
+
+        self.p = p
+        self.m = m
+        self.kernel_constraint = kernel_constraint
+        self.kernel_regularizer = kernel_regularizer
+        self.lam = lam  # (NEW) used for the regularization part
+        self.conv_layers = []
+
+        if filter_bank:
+            # (NEW) used for the regularization part
+            self.uses_filter_bank = True
+            for i in range(m):
+                layer = CustomConv1D(
+                    filters=1,
+                    filter_bank=filter_bank,
+                    kernel_constraint=kernel_constraint,
+                    kernel_regularizer=kernel_regularizer,
+                    name='customConv{}'.format(i),
+                )
+                self.conv_layers.append(layer)
+        else:
+            for i in range(m):
+                # (NEW) used for the regularization part
+                self.uses_filter_bank = False
+                layer = Conv1D(
+                    filters=1,
+                    kernel_size=p,
+                    use_bias=False,
+                    kernel_constraint=kernel_constraint,
+                    kernel_regularizer=kernel_regularizer,
+                    name='conv{}'.format(i),
+                )
+                self.conv_layers.append(layer)
+
+    def call(self, x):
+        if self.m == 1:
+            return self.conv_layers[0](x)
+        else:
+            split_inputs = tf.split(x, num_or_size_splits=self.m, axis=-1)
+            split_outputs = [self.conv_layers[i](
+                split_inputs[i]) for i in range(self.m)]
+            joined_outputs = Concatenate()(split_outputs)
+            return joined_outputs
+
+    def train_step(self, inputs):
+        X, Y = inputs
+
+        assert X.shape[1] == Y.shape[1], "Size of X and Y should be the same shape but found, {} vs {}".format(
+            X.shape[1], Y.shape[1])
+        X_padded = tf.pad(
+            X,
+            paddings=[[0, 0], [self.p-1, 0], [0, 0]],
+        )
+        
+        with tf.GradientTape() as tape:
+            Y_hat = self(X_padded)
+            loss = self.loss(Y, Y_hat)
+                
+            #####################################
+            # (NEW) Code for regularization
+            #####################################
+            m = len(self.conv_layers)
+            n = self.conv_layers[0].trainable_variables[0].shape[0]
+
+            if self.uses_filter_bank:
+                loss_regularizer = 0
+
+                for i in range(m):
+                    for j in range(n-1):
+                        loss_regularizer += (self.conv_layers[i].trainable_variables[0][j] - self.conv_layers[i].trainable_variables[0][j+1]) ** 2
+
+                loss += self.lam*loss_regularizer
+
+            #####################################
+
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(
+            zip(gradients, self.trainable_variables))
+
+        for var in self.trainable_variables:
+            var.assign(tf.math.maximum(0., var))
+        
+        self.compiled_metrics.update_state(Y, Y_hat)
+        return {m.name: m.result() for m in self.metrics}
 
 
 class ModelGamma(tf.keras.Model):
@@ -177,7 +283,7 @@ class ModelGamma(tf.keras.Model):
 
         with tf.GradientTape() as tape:
             Y_hat = self(X_padded)
-            loss = self.loss(Y, Y_hat)
+            loss = self.loss(Y[:,self.p:,:], Y_hat[:,self.p:,:])
             for i in range(self.m):
                 loss -= self.lam * tf.reduce_sum(tf.math.log(self.conv_layers[i].kernel))
 
